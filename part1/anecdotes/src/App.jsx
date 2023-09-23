2917a58976a21ea4965f0ecbfa85f06a36d3939b
dmarie777 <deimarie777@gmail.com>
2023-09-23 09:45:57 -0500

    Solution for exercise 1.14

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -11,26 +11,39 @@ function App() {
       'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
       'The only way to go fast, is to go well.'
     ]
-    const [selected, setSelected] = useState(0)
-    const [vote, setVote] = useState(Array.from({length: anecdotes.length}, x => 0))
+    const [selected, setSelected] = useState(0);
+    const [vote, setVote] = useState(Array.from({length: anecdotes.length}, x => 0));
   
-    const randomNum = () => Math.floor(Math.random() * anecdotes.length )
+    const randomNum = () => Math.floor(Math.random() * anecdotes.length );
     const handleSelection = () => {
-      setSelected(randomNum())
+      setSelected(randomNum());
     }
     const handleVote = () => {
-      let copy = [...vote]
-      copy[selected] += 1
-      setVote(copy)
+      let copy = [...vote];
+      copy[selected] += 1;
+      setVote(copy);
     }
+    let max = 0;
+    let indexMax = '';
+    vote.forEach( (e,i)=> {
+      if (max < e) {
+        max =e;
+        indexMax = [i];
+      }}
+      )
 
   return (
     <>
       <div>
+        <h2>Anecdote of the day</h2>
         <p>{anecdotes[selected]}</p>
         <p>has {vote[selected]} votes</p>
         <button onClick={handleSelection } >next anecdote</button>
         <button onClick={handleVote} >vote</button>
+
+        <h2>Anecdote with most votes</h2>
+        <p>{anecdotes[indexMax]}</p>
+        <p>has {Math.max(...vote)} votes</p>
       </div>
     </>
   )
